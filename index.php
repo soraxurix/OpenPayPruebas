95419dda1d43b7b11df9ee51d255b6463ee9a3e7
soraxurix <71204059+soraxurix@users.noreply.github.com>
2021-10-18 20:35:38 -0500

    Arreglando errores y añadiendo comentarios

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <script type='text/javascript' src="https://openpay.s3.amazonaws.com/openpay-data.v1.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
 
-    <!-- <link rel="stylesheet" type="text/css" href="styles.css"> -->
     <!-- Cargamos Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -15,6 +14,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
   <body>
+    
     <form action="#" method="POST" id="payment-form"><input type="hidden" name="token_id" id="token_id">
       <div class="pymnt-itm card active">
         <h2>Tarjeta de crédito o débito</h2>
@@ -33,16 +33,13 @@
             </div>
           </div>
           <div class="openpay">
-            
+
             <div class="shield">Tus pagos se realizan de forma segura con encriptación de 256 bits</div>
           </div>
           <button class="btn btn-primary" id="pay-button">Pagar</a>
         </div>
       </div>
     </form>
-    <!-- <div class="bkng-tb-cntnt">
-      <div class="pymnts">
-      </div>
-    </div> -->
+
   </body>
 </html>
